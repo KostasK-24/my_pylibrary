7b5049798be47f41e03e636b1f300240ed733130
KostasK-24 <gkaralis@tuc.gr>
2025-12-03 20:41:57 +0200

    setup change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="stand_brary",
-    version="1.0.0",
+    version="1.2.0",
     author="Kostas",
     author_email="gkaralis@tuc.gr",
     description="A collection of utility functions for various calculations and data processing",
